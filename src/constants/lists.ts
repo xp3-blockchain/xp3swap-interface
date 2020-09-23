a2675d13a8015de41196f5fb8dba7c7d4039df49
guyaqi <1360668777@qq.com>
2020-09-23 21:07:18 +0800

    color change

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,7 +1,9 @@
 // the Uniswap Default token list lives here
 export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
 
+
+
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
-  "https://raw.githubusercontent.com/xp3-blockchain/xp3-tokenlist/master/rinkeby-tokenlist.json",
+  (process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'http://xp3swap.mxito3.com/') + '/rinkeby-tokenlist.json',
 ]
--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -59,14 +59,17 @@ export function colors(darkMode: boolean): Colors {
     advancedBG: darkMode ? 'rgba(0,0,0,0.1)' : 'rgba(255,255,255,0.6)',
 
     //primary colors
-    primary1: darkMode ? '#2172E5' : '#ff007a',
+    // primary1: darkMode ? '#2172E5' : '#ff007a',
+    primary1: darkMode ? '#2172E5' : '#2172E5',
     primary2: darkMode ? '#3680E7' : '#FF8CC3',
     primary3: darkMode ? '#4D8FEA' : '#FF99C9',
     primary4: darkMode ? '#376bad70' : '#F6DDE8',
-    primary5: darkMode ? '#153d6f70' : '#FDEAF1',
+    // primary5: darkMode ? '#153d6f70' : '#FDEAF1',
+    primary5: darkMode ? '#153d6f70' : '#eaf0fd',
 
     // color text
-    primaryText1: darkMode ? '#6da8ff' : '#ff007a',
+    // primaryText1: darkMode ? '#6da8ff' : '#ff007a',
+    primaryText1: darkMode ? '#2172E5' : '#2172E5',
 
     // secondary colors
     secondary1: darkMode ? '#2172E5' : '#ff007a',
@@ -214,7 +217,7 @@ body {
   background-position: 0 -30vh;
   background-repeat: no-repeat;
   background-image: ${({ theme }) =>
-    `radial-gradient(50% 50% at 50% 50%, ${transparentize(0.9, theme.primary1)} 0%, ${transparentize(
+    `radial-gradient(50% 50% at 50% 50%, ${transparentize(0.8, theme.primary1)} 0%, ${transparentize(
       1,
       theme.bg1
     )} 100%)`};
